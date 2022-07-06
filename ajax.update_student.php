64de46caf0b77273bff295fcf5d140a5d666bec2
boubacarmdg <71887035+boubacarmdg@users.noreply.github.com>
2022-07-06 00:40:09 +0000

    Delete ajax.update_student.php

--- ajax.update_student.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "clientSOAP.php";
-$params = new stdClass();
-$params->id = $_POST['id'];
-$params->nom = strtoupper($_POST['nom']);
-$params->prenom = $_POST['prenom'];
-$res = $clientSOAP->__soapCall("updateEtudiant", array($params));
-print $res->return;
