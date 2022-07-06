7735273f473d89ed36fc92e3a3871be3a226d6d8
boubacarmdg <71887035+boubacarmdg@users.noreply.github.com>
2022-07-06 00:39:53 +0000

    Delete ajax.add_student.php

--- ajax.add_student.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "clientSOAP.php";
-$params = new stdClass();
-$params->nom = strtoupper($_POST['nom']);
-$params->prenom = $_POST['prenom'];
-$res = $clientSOAP->__soapCall("addEtudiant", array($params));
-print $res->return;
